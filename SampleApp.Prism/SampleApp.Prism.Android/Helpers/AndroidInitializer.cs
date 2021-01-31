622414f4672372e484adcecc90c43bcf9649dfa5
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-01-31 19:54:35 +0530

    AndroidInitializer added.

---
+++ SampleApp.Prism/SampleApp.Prism.Android/Helpers/AndroidInitializer.cs
@@ -0,0 +1,12 @@
+﻿using Prism;
+using Prism.Ioc;
+
+namespace SampleApp.Prism.Droid.Helpers
+{
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
