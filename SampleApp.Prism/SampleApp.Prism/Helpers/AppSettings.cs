213c67b61ec60cf8a7398436c6c83a920dd73222
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-01 11:53:37 +0530

    AppSettings added.

---
+++ SampleApp.Prism/SampleApp.Prism/Helpers/AppSettings.cs
@@ -0,0 +1,36 @@
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+
+namespace SampleApp.Prism.Helpers
+{
+    public class AppSettings
+    {
+        private static ISettings Settings => CrossSettings.Current;
+
+        private const bool DefaultIsLoggedIn = false;
+
+        public static bool IsLoggedIn
+        {
+            get => Settings.GetValueOrDefault(nameof(IsLoggedIn), DefaultIsLoggedIn);
+            set => Settings.AddOrUpdateValue(nameof(IsLoggedIn), value);
+        }
+
+
+        private const bool DefaultIsFingerprintSet = false;
+
+        public static bool IsFingerprintSet
+        {
+            get => Settings.GetValueOrDefault(nameof(IsFingerprintSet), DefaultIsFingerprintSet);
+            set => Settings.AddOrUpdateValue(nameof(IsFingerprintSet), value);
+        }
+
+        private const string DefaultUserEmail = "";
+
+        public static string UserEmail
+        {
+            get => Settings.GetValueOrDefault(nameof(UserEmail), DefaultUserEmail);
+            set => Settings.AddOrUpdateValue(nameof(UserEmail), value);
+        }
+
+    }
+}
