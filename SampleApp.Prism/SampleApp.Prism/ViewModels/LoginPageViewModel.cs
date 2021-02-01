494490f890bfb3cae36efc88ec8d72833348f378
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-01 11:49:14 +0530

    LoginPage and LoginViewModel updated.

--- SampleApp.Prism/SampleApp.Prism/ViewModels/LoginPageViewModel.cs
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/LoginPageViewModel.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Prism.Navigation;
+using Prism.Services;
+using Prism.Services.Dialogs;
+using Prism.Unity;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SampleApp.Prism.ViewModels
 {
-    class LoginPageViewModel
+    public class LoginPageViewModel : BaseViewModel
     {
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                RaisePropertyChanged();
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                RaisePropertyChanged();
+            }
+        }
+        public ICommand SubmitCommand { protected set; get; }
+        public ICommand LoginCommand { get; set; }
+
+
+        public LoginPageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService)
+            : base(navigationService, dialogService, pageDialogService)
+        {
+            LoginCommand = new Command(LoginClicked);
+        }
+
+       
+
+        private async void LoginClicked(object obj)
+        {
+            //try
+            //{
+            //    AppSettings.IsLoggedIn = true;
+
+            //    MessagingCenter.Send<LoginPageViewModel, string>(this, "UserLoggedIn", "test");
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    await _pageDialogService.DisplayAlertAsync("Error", "Something went wrong" + ex.Message, "Ok");
+            //}
+        }
     }
 }
