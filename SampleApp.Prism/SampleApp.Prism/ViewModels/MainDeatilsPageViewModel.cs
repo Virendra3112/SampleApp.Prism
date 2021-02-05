b49ebb727bc2eae27bb45724d69e0687f4c0c46e
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-05 18:35:02 +0530

    MainDeatilsPageViewModel updated.

--- SampleApp.Prism/SampleApp.Prism/ViewModels/MainDeatilsPageViewModel.cs
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/MainDeatilsPageViewModel.cs
@@ -1,34 +1,81 @@
 ﻿using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Navigation;
-using Prism.Services;
-using Prism.Services.Dialogs;
+using SampleApp.Prism.Models;
+using SampleApp.Prism.Views;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
 
 namespace SampleApp.Prism.ViewModels
 {
-    public class MainDeatilsPageViewModel : BaseViewModel
+    public class MainDeatilsPageViewModel : BindableBase//BaseViewModel
     {
-       // INavigationService _navigationService;
-        public DelegateCommand<string> OnNavigateCommand { get; set; }
+        private INavigationService _navigationService;
 
-        public MainDeatilsPageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService)
-            : base(navigationService, dialogService, pageDialogService)
+        public ObservableCollection<MyMenuItem> MenuItems { get; set; }
+
+        private MyMenuItem selectedMenuItem;
+        public MyMenuItem SelectedMenuItem
         {
-            //_navigationService = navigationService;
-            OnNavigateCommand = new DelegateCommand<string>(NavigateToPage);
+            get => selectedMenuItem;
+            set => SetProperty(ref selectedMenuItem, value);
         }
 
-        private async void NavigateToPage(string obj)
-        {
-            try
-            {
-                await _navigationService.NavigateAsync(new Uri(obj, UriKind.Relative));
-            }
-            catch (Exception)
-            {
+        public DelegateCommand NavigateCommand { get; private set; }
 
-                throw;
-            }
+        public MainDeatilsPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            MenuItems = new ObservableCollection<MyMenuItem>();
+
+            MenuItems.Add(new MyMenuItem()
+            {
+                Icon = "icon",
+                PageName = nameof(FirstDetailsPage),
+                Title = "View A"
+            });
+
+            MenuItems.Add(new MyMenuItem()
+            {
+                Icon = "icon",
+                PageName = nameof(SecondDetailsPage),
+                Title = "View B"
+            });
+
+            NavigateCommand = new DelegateCommand(Navigate);
         }
+
+        async void Navigate()
+        {
+            await _navigationService.NavigateAsync(nameof(NavigationPage) + "/" + SelectedMenuItem.PageName);
+        }
+
+
+        //// INavigationService _navigationService;
+        // public DelegateCommand<string> OnNavigateCommand { get; set; }
+
+        // public MainDeatilsPageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService)
+        //     : base(navigationService, dialogService, pageDialogService)
+        // {
+        //     //_navigationService = navigationService;
+        //     OnNavigateCommand = new DelegateCommand<string>(NavigateToPage);
+        // }
+
+        // private async void NavigateToPage(string obj)
+        // {
+        //     try
+        //     {
+        //         await _navigationService.NavigateAsync(new Uri(obj, UriKind.Relative));
+        //     }
+        //     catch (Exception)
+        //     {
+
+        //         throw;
+        //     }
+        // }
     }
 }
