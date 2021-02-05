a5be096bfcca245e1a702b7585db64487a3eb950
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-05 18:31:20 +0530

    MyMenuItem added.

---
+++ SampleApp.Prism/SampleApp.Prism/Models/MyMenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleApp.Prism.Models
+{
+    public class MyMenuItem
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string PageName { get; set; }
+    }
+}
