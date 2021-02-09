bdee4fba05805c5543ffb6f2133380823bdf1222
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-09 19:52:01 +0530

    HomePageViewModel added.

---
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using Prism.Services;
+using Prism.Services.Dialogs;
+using SampleApp.Prism.Models;
+using SampleApp.Prism.Views;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace SampleApp.Prism.ViewModels
+{
+    public class HomePageViewModel : BaseViewModel
+    {
+        public HomePageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService)
+              : base(navigationService, dialogService, pageDialogService)
+        {
+        }
+    }
+}
