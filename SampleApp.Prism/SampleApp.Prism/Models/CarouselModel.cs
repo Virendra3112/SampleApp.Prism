d8013815e2e90bbf54b102d300b831e825b569c5
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-10 15:37:57 +0530

    CarouselModel added.

---
+++ SampleApp.Prism/SampleApp.Prism/Models/CarouselModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleApp.Prism.Models
+{
+    public class CarouselModel
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsCurrent { get; set; }
+    }
+}
