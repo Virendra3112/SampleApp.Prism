d3106d0bec53e362472dce1e6b158d91eab4f03b
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-12 22:59:13 +0530

    ThemeHelper added for iOS.

---
+++ SampleApp.Prism/SampleApp.Prism.iOS/CustomRendrers/ThemeHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SampleApp.Prism.CustomControls;
+using SampleApp.Prism.Models.Enum;
+
+namespace SampleApp.Prism.iOS.CustomRendrers
+{
+    public class ThemeHelper : IAppTheme
+    {
+        public void SetAppTheme(Theme theme)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
