29fdb710c8543e87fd28708c3ff55e1c4671060f
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-12 23:00:47 +0530

    ThemeHelper updated.

--- SampleApp.Prism/SampleApp.Prism.iOS/CustomRendrers/ThemeHelper.cs
+++ SampleApp.Prism/SampleApp.Prism.iOS/CustomRendrers/ThemeHelper.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using SampleApp.Prism.CustomControls;
+using SampleApp.Prism.iOS.CustomRendrers;
 using SampleApp.Prism.Models.Enum;
+using SampleApp.Prism.Resources;
+using Xamarin.Forms;
 
+[assembly: Dependency(typeof(ThemeHelper))]
 namespace SampleApp.Prism.iOS.CustomRendrers
 {
     public class ThemeHelper : IAppTheme
-    {
-        public void SetAppTheme(Theme theme)
+    {        public void SetAppTheme(Theme theme)
         {
-            throw new NotImplementedException();
+            if (theme == Theme.Dark)
+            {
+                if (App.AppTheme == Theme.Dark)
+                    return;
+                App.Current.Resources = new DarkTheme();
+            }
+            else
+            {
+                if (App.AppTheme != Theme.Dark)
+                    return;
+                App.Current.Resources = new LightTheme();
+            }
+            App.AppTheme = theme;
         }
     }
 }
