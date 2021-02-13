a792465cc010675e25ccc8cb396e519a95396e5e
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-13 17:06:42 +0530

    FirstTabPageViewModel, SecondTabPageViewModel updated.

---
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/FirstTabPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Navigation;
+using Prism.Services;
+using Prism.Services.Dialogs;
+
+namespace SampleApp.Prism.ViewModels
+{
+    public class FirstTabPageViewModel : BaseViewModel
+    {
+        public FirstTabPageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService) : base(navigationService, dialogService, pageDialogService)
+        {
+        }
+    }
+}
---
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/SecondTabPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Navigation;
+using Prism.Services;
+using Prism.Services.Dialogs;
+
+namespace SampleApp.Prism.ViewModels
+{
+    public class SecondTabPageViewModel : BaseViewModel
+    {
+        public SecondTabPageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService) : base(navigationService, dialogService, pageDialogService)
+        {
+        }
+    }
+}
