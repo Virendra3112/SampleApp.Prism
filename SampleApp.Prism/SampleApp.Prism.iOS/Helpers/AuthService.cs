ab6f2831fa45fec3f1ef88118102661f9622f5e7
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-16 11:47:17 +0530

    AuthService added for iOS.

---
+++ SampleApp.Prism/SampleApp.Prism.iOS/Helpers/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Foundation;
+using SampleApp.Prism.CustomControls;
+using SampleApp.Prism.iOS.Helpers;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AuthService))]
+namespace SampleApp.Prism.iOS.Helpers
+{
+    public class AuthService : IAuthService
+    {
+        public Task<bool> AuthenticateUserIDWithTouchID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool fingerprintEnabled()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAuthenticationType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
