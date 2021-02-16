ec161f0b9b6453a8867356d40390ee1c05b381ee
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-16 11:49:32 +0530

    AuthService updated for iOS.

--- SampleApp.Prism/SampleApp.Prism.iOS/Helpers/AuthService.cs
+++ SampleApp.Prism/SampleApp.Prism.iOS/Helpers/AuthService.cs
@@ -1,21 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Foundation;
 using SampleApp.Prism.CustomControls;
 using SampleApp.Prism.iOS.Helpers;
 using UIKit;
+using LocalAuthentication;
+using Xamarin.Forms;
 
 [assembly: Xamarin.Forms.Dependency(typeof(AuthService))]
 namespace SampleApp.Prism.iOS.Helpers
 {
     public class AuthService : IAuthService
     {
+        public AuthService()
+        {
+
+        }
+
+
         public Task<bool> AuthenticateUserIDWithTouchID()
         {
-            throw new NotImplementedException();
+            bool outcome = false;
+            var tcs = new TaskCompletionSource<bool>();
+
+            var context = new LAContext();
+            if (context.CanEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, out NSError AuthError))
+            {
+
+
+                var replyHandler = new LAContextReplyHandler((success, error) => {
+
+                    Device.BeginInvokeOnMainThread(() => {
+                        if (success)
+                        {
+                            outcome = true;
+                        }
+                        else
+                        {
+                            outcome = false;
+                        }
+                        tcs.SetResult(outcome);
+                    });
+
+                });
+                //This will call both TouchID and FaceId 
+                context.EvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, "Login with touch ID", replyHandler);
+            };
+            return tcs.Task;
         }
 
         public bool fingerprintEnabled()
@@ -23,9 +54,32 @@ namespace SampleApp.Prism.iOS.Helpers
             throw new NotImplementedException();
         }
 
+        private static int GetOsMajorVersion()
+        {
+            return int.Parse(UIDevice.CurrentDevice.SystemVersion.Split('.')[0]);
+        }
+
         public string GetAuthenticationType()
         {
-            throw new NotImplementedException();
+            var localAuthContext = new LAContext();
+            NSError AuthError;
+
+            if (localAuthContext.CanEvaluatePolicy(LAPolicy.DeviceOwnerAuthentication, out AuthError))
+            {
+                if (localAuthContext.CanEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, out AuthError))
+                {
+                    if (GetOsMajorVersion() >= 11 && localAuthContext.BiometryType == LABiometryType.FaceId)
+                    {
+                        return "FaceId";
+                    }
+
+                    return "TouchId";
+                }
+
+                return "PassCode";
+            }
+
+            return "None";
         }
     }
 }
