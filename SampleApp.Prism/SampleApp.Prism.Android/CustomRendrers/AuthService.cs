6a98ab2e57f7d3f2164a97bd4e419f47731fade6
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-16 11:30:12 +0530

    AuthService added to android.

---
+++ SampleApp.Prism/SampleApp.Prism.Android/CustomRendrers/AuthService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using SampleApp.Prism.CustomControls;
+using SampleApp.Prism.Droid.CustomRendrers;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AuthService))]
+namespace SampleApp.Prism.Droid.CustomRendrers
+{
+    public class AuthService : IAuthService
+    {
+        public Task<bool> AuthenticateUserIDWithTouchID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool fingerprintEnabled()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAuthenticationType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
