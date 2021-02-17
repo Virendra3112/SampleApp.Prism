2283be79253cd209b93b215340a484f4b7b28e10
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-17 09:08:13 +0530

    TouchIdAuthService added for android.

---
+++ SampleApp.Prism/SampleApp.Prism.Android/CustomRendrers/TouchIdAuthService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SampleApp.Prism.CustomControls;
+
+namespace SampleApp.Prism.Droid.CustomRendrers
+{
+    public class TouchIdAuthService : IBiometricPieAuthenticate
+    {
+        public bool CheckSDKGreater29()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterOrAuthenticate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
