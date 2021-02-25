92dad40eef55b732080ae0d7fe8eb19ae165fe87
Virendra Singh Thakur <virendra.thakur31@gmail.com>
2021-02-25 13:52:31 +0530

    DeviceAuthDetailsPageViewModel updated.

--- SampleApp.Prism/SampleApp.Prism/ViewModels/DeviceAuthDetailsPageViewModel.cs
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/DeviceAuthDetailsPageViewModel.cs
@@ -1,10 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Prism.Navigation;
+using Prism.Services;
+using Prism.Services.Dialogs;
+using SampleApp.Prism.CustomControls;
+using Xamarin.Forms;
 
 namespace SampleApp.Prism.ViewModels
 {
-    class DeviceAuthDetailsPageViewModel
+    public class DeviceAuthDetailsPageViewModel : BaseViewModel
     {
+        private string _authDetails;
+        public string AuthDetails
+        {
+            get { return _authDetails; }
+            set
+            {
+                _authDetails = value;
+                RaisePropertyChanged();
+            }
+        }
+        public DeviceAuthDetailsPageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService) : base(navigationService, dialogService, pageDialogService)
+        {
+            GetData();
+        }
+
+        private void GetData()
+        {
+            try
+            {
+                if (Device.RuntimePlatform == Device.iOS)
+                {
+                    var _authType = DependencyService.Get<IAuthService>().GetAuthenticationType();
+                    AuthDetails = "Auth Type: " + _authType;
+                }
+                if (Device.RuntimePlatform == Device.Android)
+                {
+                    //var _authType = DependencyService.Get<IAuthService>().GetAuthenticationType();
+                    AuthDetails = "Auth Type: ";
+
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+
+            }
+
+        }
     }
 }
