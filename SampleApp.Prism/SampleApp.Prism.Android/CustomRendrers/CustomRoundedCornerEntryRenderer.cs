b8ae49429ef8780bfc97e524d0d30dd4e4b4b74b
Virendra <virendra.thakur31@gmail.com>
2021-03-05 11:39:31 +0530

    Custom rounded corner entry conytol added.

---
+++ SampleApp.Prism/SampleApp.Prism.Android/CustomRendrers/CustomRoundedCornerEntryRenderer.cs
@@ -0,0 +1,66 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Android.Util;
+using SampleApp.Prism.CustomControls;
+using SampleApp.Prism.Droid.CustomRendrers;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomRoundedCornerEntry), typeof(CustomRoundedCornerEntryRenderer))]
+namespace SampleApp.Prism.Droid.CustomRendrers
+{
+    public class CustomRoundedCornerEntryRenderer : EntryRenderer
+    {
+        public CustomRoundedCornerEntryRenderer(Context context) : base(context)
+        {
+            AutoPackage = false;
+        }
+        //protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        //{
+        //    base.OnElementChanged(e);
+        //    if (Control != null)
+        //    {
+        //        Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+        //    }
+        //}
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                var view = (CustomRoundedCornerEntry)Element;
+                if (view.IsCurvedCornersEnabled)
+                {
+                    // creating gradient drawable for the curved background  
+                    var _gradientBackground = new GradientDrawable();
+                    _gradientBackground.SetShape(ShapeType.Rectangle);
+                    _gradientBackground.SetColor(view.BackgroundColor.ToAndroid());
+
+                    // Thickness of the stroke line  
+                    _gradientBackground.SetStroke(view.BorderWidth, view.BorderColor.ToAndroid());
+
+                    // Radius for the curves  
+                    _gradientBackground.SetCornerRadius(
+                        DpToPixels(this.Context, Convert.ToSingle(view.CornerRadius)));
+
+                    // set the background of the   
+                    Control.SetBackground(_gradientBackground);
+                }
+                // Set padding for the internal text from border  
+                Control.SetPadding(
+                    (int)DpToPixels(this.Context, Convert.ToSingle(12)), Control.PaddingTop,
+                    (int)DpToPixels(this.Context, Convert.ToSingle(12)), Control.PaddingBottom);
+            }
+        }
+        public static float DpToPixels(Context context, float valueInDp)
+        {
+            DisplayMetrics metrics = context.Resources.DisplayMetrics;
+            return TypedValue.ApplyDimension(ComplexUnitType.Dip, valueInDp, metrics);
+        }
+
+
+
+    }
+}
---
+++ SampleApp.Prism/SampleApp.Prism/CustomControls/CustomRoundedCornerEntry.cs
@@ -0,0 +1,47 @@
+﻿using Xamarin.Forms;
+
+namespace SampleApp.Prism.CustomControls
+{
+    public class CustomRoundedCornerEntry : Entry
+    {
+
+        //Todo
+        public static readonly BindableProperty BorderColorProperty =
+        BindableProperty.Create(nameof(BorderColor),
+            typeof(Color), typeof(CustomRoundedCornerEntry), Color.Gray);
+        // Gets or sets BorderColor value  
+        public Color BorderColor
+        {
+            get => (Color)GetValue(BorderColorProperty);
+            set => SetValue(BorderColorProperty, value);
+        }
+
+        public static readonly BindableProperty BorderWidthProperty =
+        BindableProperty.Create(nameof(BorderWidth), typeof(int),
+            typeof(CustomRoundedCornerEntry), Device.OnPlatform<int>(1, 2, 2));
+        // Gets or sets BorderWidth value  
+        public int BorderWidth
+        {
+            get => (int)GetValue(BorderWidthProperty);
+            set => SetValue(BorderWidthProperty, value);
+        }
+        public static readonly BindableProperty CornerRadiusProperty =
+        BindableProperty.Create(nameof(CornerRadius),
+            typeof(double), typeof(CustomRoundedCornerEntry), Device.OnPlatform<double>(6, 7, 7));
+        // Gets or sets CornerRadius value  
+        public double CornerRadius
+        {
+            get => (double)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+        public static readonly BindableProperty IsCurvedCornersEnabledProperty =
+        BindableProperty.Create(nameof(IsCurvedCornersEnabled),
+            typeof(bool), typeof(CustomRoundedCornerEntry), true);
+        // Gets or sets IsCurvedCornersEnabled value  
+        public bool IsCurvedCornersEnabled
+        {
+            get => (bool)GetValue(IsCurvedCornersEnabledProperty);
+            set => SetValue(IsCurvedCornersEnabledProperty, value);
+        }
+    }
+}
