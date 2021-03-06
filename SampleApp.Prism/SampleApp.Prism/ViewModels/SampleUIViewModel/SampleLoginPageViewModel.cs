0c473bdd9bca3e41c1f078e7f61bdaf52aebe400
Virendra <virendra.thakur31@gmail.com>
2021-03-06 22:46:45 +0530

    Sample login page and view model updated.

--- SampleApp.Prism/SampleApp.Prism/ViewModels/SampleUIViewModel/SampleLoginPageViewModel.cs
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/SampleUIViewModel/SampleLoginPageViewModel.cs
@@ -1,13 +1,26 @@
 ﻿using Prism.Navigation;
 using Prism.Services;
 using Prism.Services.Dialogs;
+using SampleApp.Prism.Views.SampleUIPages;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SampleApp.Prism.ViewModels.SampleUIViewModel
 {
     public class SampleLoginPageViewModel : BaseViewModel
     {
+        public ICommand LoginCommand { get; set; }
+
         public SampleLoginPageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService) : base(navigationService, dialogService, pageDialogService)
         {
+            LoginCommand = new Command(LoginClicked);
+
+        }
+
+        private async void LoginClicked(object obj)
+        {
+           await _navigationService.NavigateAsync(nameof(SampleDashboardPage));
+
         }
     }
 }
