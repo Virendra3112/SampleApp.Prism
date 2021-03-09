d6a347219e67e54e9eeebe3e5a59f52d7c70dfa6
Virendra <virendra.thakur31@gmail.com>
2021-03-09 12:01:33 +0530

    Sample dashboard page and viewmodel updated.

--- SampleApp.Prism/SampleApp.Prism/ViewModels/SampleUIViewModel/SampleDashboardPageViewModel.cs
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/SampleUIViewModel/SampleDashboardPageViewModel.cs
@@ -1,13 +1,56 @@
 ﻿using Prism.Navigation;
 using Prism.Services;
 using Prism.Services.Dialogs;
+using SampleApp.Prism.Views.SampleUIPages;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SampleApp.Prism.ViewModels.SampleUIViewModel
 {
     public class SampleDashboardPageViewModel : BaseViewModel
     {
+        public ICommand SelectCommand { get; set; }
+
         public SampleDashboardPageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService) : base(navigationService, dialogService, pageDialogService)
         {
+            SelectCommand = new Command(OnTileClicked);
+        }
+
+        private async void OnTileClicked(object obj)
+        {
+            try
+            {
+                switch (obj.ToString())
+                {
+                    case "Check-In":
+
+                        await _navigationService.NavigateAsync(nameof(SampleCheckinOutTypePage));
+
+                        break;
+
+                    case "Check-Out":
+                        await _navigationService.NavigateAsync(nameof(SampleCheckinOutTypePage));
+
+                        break;
+
+                    case "Logs":
+
+                        break;
+
+                    case "Supervision":
+
+                        break;
+
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+
         }
     }
 }
