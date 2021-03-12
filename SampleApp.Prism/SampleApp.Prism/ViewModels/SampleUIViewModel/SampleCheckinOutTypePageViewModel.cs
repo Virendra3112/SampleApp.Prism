1cff8acf0751ce5b1858b4afc3b3a4061a86af9d
Virendra <virendra.thakur31@gmail.com>
2021-03-12 12:18:23 +0530

    SampleCheckinOutTypePage viewmodel updated.

--- SampleApp.Prism/SampleApp.Prism/ViewModels/SampleUIViewModel/SampleCheckinOutTypePageViewModel.cs
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/SampleUIViewModel/SampleCheckinOutTypePageViewModel.cs
@@ -1,13 +1,55 @@
 ﻿using Prism.Navigation;
 using Prism.Services;
 using Prism.Services.Dialogs;
+using SampleApp.Prism.Models.Enum;
+using SampleApp.Prism.Views.SampleUIPages;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 
 namespace SampleApp.Prism.ViewModels.SampleUIViewModel
 {
     public class SampleCheckinOutTypePageViewModel : BaseViewModel
     {
+        public ICommand SelectCommand { get; set; }
+
         public SampleCheckinOutTypePageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService) : base(navigationService, dialogService, pageDialogService)
         {
+            SelectCommand = new Command<string>(OnTileClicked);
+        }
+
+        private async void OnTileClicked(string obj)
+        {
+            try
+            {
+                switch (obj)
+                {
+                    case nameof(Tiles.Visitors):
+                        var navParameters = new NavigationParameters
+                            {
+                                { "TileName", nameof(Tiles.CheckIn) }
+                            };
+                        //await _navigationService.NavigateAsync(nameof(SampleCheckinOutTypePage), navParameters);
+
+                        break;
+
+                    case nameof(Tiles.Residents):
+                        // await _navigationService.NavigateAsync(nameof(SampleCheckinOutTypePage));
+
+                        break;
+
+                    case nameof(Tiles.ServiceProviders):
+
+                        break;
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
