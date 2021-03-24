61367b6e0066311bfafbb94b79ef5059ccbdcadc
Virendra <virendra.thakur31@gmail.com>
2021-03-24 14:22:23 +0530

    PanPinchSample page and viewmodel updated.

--- SampleApp.Prism/SampleApp.Prism/ViewModels/PanPinchSamplePageViewModel.cs
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/PanPinchSamplePageViewModel.cs
@@ -6,8 +6,20 @@ namespace SampleApp.Prism.ViewModels
 {
     public class PanPinchSamplePageViewModel : BaseViewModel
     {
+        private string _imageUrl;
+        public string ImageUrl
+        {
+            get { return _imageUrl; }
+            set
+            {
+                _imageUrl = value;
+                RaisePropertyChanged();
+            }
+        }
         public PanPinchSamplePageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService) : base(navigationService, dialogService, pageDialogService)
         {
+            ImageUrl = "https://cdn.shopify.com/s/files/1/0496/1029/files/Freesample.svg";
         }
+
     }
 }
