4bf464ee46d2e933a990e04077f8f4b4e1849850
Virendra <virendra.thakur31@gmail.com>
2021-03-24 13:58:32 +0530

    PanPinchSamplePage and ViewModel added.

---
+++ SampleApp.Prism/SampleApp.Prism/ViewModels/PanPinchSamplePageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Navigation;
+using Prism.Services;
+using Prism.Services.Dialogs;
+
+namespace SampleApp.Prism.ViewModels
+{
+    public class PanPinchSamplePageViewModel : BaseViewModel
+    {
+        public PanPinchSamplePageViewModel(INavigationService navigationService, IDialogService dialogService, IPageDialogService pageDialogService) : base(navigationService, dialogService, pageDialogService)
+        {
+        }
+    }
+}
